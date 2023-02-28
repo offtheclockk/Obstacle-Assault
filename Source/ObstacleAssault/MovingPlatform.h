d512fcb60345504a0b4da88f9b18ae587b8e719f
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2023-02-28 13:33:16 -0800

    VS Code

---
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSAULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	FVector MyVector = FVector(-14000, -2340, 4050);
+
+	UPROPERTY(EditAnywhere)
+	float MyX = 0;
+
+	UPROPERTY(EditAnywhere)
+	float MyY = 4;
+
+	UPROPERTY(EditAnywhere)
+	float MyZ = 5;
+};
